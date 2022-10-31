b197a5241ea463fdbc777a9c2588baf6b0328a67
Monserrat Díaz <monse@nubiral.com>
2022-10-31 15:21:12 -0300

    Show notification component depending on notification slice value

--- src/App.js
+++ src/App.js
@@ -4,16 +4,18 @@ import { uiActions } from "./store/ui-slice";
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
+import Notification from "./components/UI/Notification";
 
 function App() {
   const dispatch = useDispatch();
   const showCart = useSelector((state) => state.ui.cartIsVisible);
   const cart = useSelector((state) => state.cart);
+  const notification = useSelector((state) => state.ui.notification);
 
   const API_URL = process.env.REACT_APP_API_URL_FIREBASE;
 
   useEffect(() => {
-    sendCartData = async () => {
+    const sendCartData = async () => {
       dispatch(
         uiActions.showNotification({
           status: "pending",
@@ -52,10 +54,19 @@ function App() {
   }, [cart]);
 
   return (
-    <Layout>
-      {showCart && <Cart />}
-      <Products />
-    </Layout>
+    <>
+      {notification && (
+        <Notification
+          status={notification.status}
+          title={notification.title}
+          message={notification.message}
+        />
+      )}
+      <Layout>
+        {showCart && <Cart />}
+        <Products />
+      </Layout>
+    </>
   );
 }
 
