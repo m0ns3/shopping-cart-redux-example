48bd7469935789b1a7fa4266525c3f4b644dcd65
Monserrat Díaz <monse@nubiral.com>
2022-10-31 11:52:33 -0300

    Make an http request to update cart data

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { useSelector } from "react-redux";
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
@@ -5,6 +6,18 @@ import Products from "./components/Shop/Products";
 
 function App() {
   const showCart = useSelector((state) => state.ui.cartIsVisible);
+  const cart = useSelector((state) => state.cart);
+
+  const API_URL = process.env.REACT_APP_API_URL_FIREBASE;
+
+  useEffect(() => {
+    fetch(`${API_URL}/cart.json`, {
+      method: "PUT",
+      body: JSON.stringify(cart),
+    });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [cart]);
+
   return (
     <Layout>
       {showCart && <Cart />}
